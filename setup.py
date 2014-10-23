7a1634d4eca45d9db96c7abe4acf366c01bfc5b3
Alex Haslehurst <alexander.h@alpharooms.com>
2014-10-23 13:55:15 +0100

    Fixed project setup

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
     name='media-scrapers',
     version='0.1',
-    packages=['axh', 'axh.yts'],
+    packages=['axh', 'axh.media', 'axh.media.yts'],
     url='axh.pwnz.org',
     license='',
     author='Alex Haslehurst',
     author_email='alex.haslehurst@gmail.com',
-    description='Scraper for yts.re',
+    description='Collection of media scrapers',
     requires=['humanfriendly', 'beautifulsoup4', 'html5lib'],
     install_requires=['humanfriendly', 'beautifulsoup4', 'html5lib']
 )
